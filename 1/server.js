e02df4c18c6a96c5076685dd2b1f486ed1411eba
Alex <lesnar1@mail.ru>
2023-01-24 22:08:59 +0300

    Lesson_1_done

--- 1/server.js
+++ 1/server.js
@@ -1,13 +1,20 @@
 const http = require('http');
+const fs = require('fs')
 
 const host = 'localhost';
 const port = 8000
 
 const requestListener = (req, res) => {
     if (req.url === '/get' && req.method === 'GET' ) { 
-        
+        return res.end("К сожалению, ни на занятии, ни в методичке нет достаточной информации для выполнения этого задания в полной мере");   
+    } else if ( req.url === '/delete' && req.method === 'DELETE') {
         res.writeHead(200);
-        return res.end('Success!');
+        return res.end('Success');
+    } else if ( req.url === '/post' && req.method === 'POST') {
+        res.writeHead(200);
+        return res.end('Success');
+    } else if ( req.url === '/redirect' && req.method === 'GET') {
+        return res.end(`Сайт переехал. Совсем. Навсегда. http://${host}:${port}/redirected  `)
     }
     res.writeHead(405);
     return res.end('HTTP method not allowed');  
